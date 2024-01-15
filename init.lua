b843af72d1e94844f8533107eea8843256a39778
Viktor Homeniuk <viktor.homeniuk@zenseact.com>
2024-01-15 10:40:20 +0100

    Minor cleanup

--- init.lua
+++ init.lua
@@ -264,23 +264,10 @@ require('lazy').setup({
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-    -- config = function()
-    --   require("nvim-tree").setup {}
-    -- end,
+    config = function()
+      require("nvim-tree").setup {}
+    end,
   }
-  -- NOTE: Next Step on Your Neovim Journey: Add/Configure additional "plugins" for kickstart
-  --       These are some example plugins that I've included in the kickstart repository.
-  --       Uncomment any of the lines below to enable them.
-  -- require 'kickstart.plugins.autoformat',
-  -- require 'kickstart.plugins.debug',
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    You can use this folder to prevent any conflicts with this init.lua if you're interested in keeping
-  --    up-to-date with whatever is in the kickstart repo.
-  --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --
-  --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
@@ -686,6 +673,7 @@ vim.g.loaded_netrwPlugin = 1
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
+-- Setup nvim-tree plugin with options
 require("nvim-tree").setup({
   sort = {
     sorter = "case_sensitive",
